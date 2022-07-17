5280f1a0298f13567bf2de5fc7c34ae262ac9236
Mikle <gumenyukmichael@gmail.com>
2022-07-17 22:50:52 +0300

    6. DSL c AssertJ [#504879] Подлючение dsl к jabber-у

--- src/test/java/ru/job4j/AppTest.java
+++ src/test/java/ru/job4j/AppTest.java
@@ -1,18 +1,17 @@
 package ru.job4j;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
 
-import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void whenEquals() {
+        int result = 1;
+        int expected = 1;
+        assertThat(result).isEqualTo(expected);
     }
 }
